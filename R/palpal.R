f7cc0ff92746ae9111b4e5d5fe2aa77a8a13818b
Anastasia <anastasia.lucas@pennmedicine.upenn.edu>
2019-04-19 14:27:39 -0400

    Added tol palette

--- R/palpal.R
+++ R/palpal.R
@@ -2,6 +2,8 @@
 #' export
 palpal_palettes <- list(wong = c("#000000", "#e69f00", "#56b4e9", "#009e73",
                         "#f0e442", "#0072b2", "#d55e00", "#cc79a7"),
+                        tol = c("#332288", "#AA4499", "#88CCEE", "#882255", "#44AA99",
+                                "#CC6677","#117733", "#DDCC77", "#999933"),
                         mkweb = c("#009292", "#920000", "#490092", "#db6d00", "#24ff24",
                           "#ffff6d", "#000000", "#006ddb", "#004949","#924900",
                           "#ff6db6", "#6db6ff","#b66dff", "#ffb6db","#b6dbff"))
@@ -15,6 +17,8 @@ palpal_palettes <- list(wong = c("#000000", "#e69f00", "#56b4e9", "#009e73",
 #' @examples
 #' palpal("wong")
 #' palpal("mkweb")
+#' \dontrun{ggplot(data=mtcars, aes(x=mpg, y=disp, color=factor(cyl))) +
+#' geom_point() + scale_color_manual(values=palpal("mkweb"))}
 
 palpal <- function(name, n, type = c("discrete", "continuous")) {
   type <- match.arg(type)
